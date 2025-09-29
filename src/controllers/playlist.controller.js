5865e414787503e1874cf609e98d3f2c58a69c31
y-jaga <yogeshjaga123@gmail.com>
2025-09-29 17:37:14 +0530

    feat: implement add video to playlist functionality with validation and error handling; update routes accordingly

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,6 +3,7 @@ import { Playlist } from "../models/playlist.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.models.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   //TODO: create playlist
@@ -26,7 +27,7 @@ const createPlaylist = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, video, "Playlist created successfully."));
 });
 
-//TODO: when playlist fetched in getUserPlaylists, getPlaylistById the videos filed contains objId of video should we populate them
+//TODO: when playlist fetched in getUserPlaylists, getPlaylistById the videos field contains objId of video should we populate them
 //anyone can fetch all playlist of any user
 const getUserPlaylists = asyncHandler(async (req, res) => {
   //TODO: get user playlists
@@ -69,8 +70,54 @@ const getPlaylistById = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, playlist, "playlist fetched successfully."));
 });
 
+//only owner can video in playlist
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
+  //retrieve and validate videoId and playlistId
   const { playlistId, videoId } = req.params;
+
+  if (!playlistId || !isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Valid playlistId is required.");
+  }
+  if (!videoId || !isValidObjectId(videoId)) {
+    throw new ApiError(400, "Valid videoId is required.");
+  }
+
+  //fetch playlist and video by id, and validate them as well
+  const [playlist, video] = await Promise.all([
+    Playlist.findById(playlistId),
+    Video.findById(videoId),
+  ]);
+
+  if (!playlist) {
+    throw new ApiError(404, "playlist doesn't exists.");
+  }
+  if (!video) {
+    throw new ApiError(404, "video doesn't exists.");
+  }
+
+  if (!playlist.owner.equals(req.user?._id)) {
+    throw new ApiError(403, "Only playlist owner can add video in playlist.");
+  }
+
+  const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    { $addToSet: { videos: videoId } },
+    { new: true }
+  ).populate({
+    path: "videos",
+    select: "title thumbnail videoFile duration ",
+    options: { limit: 20, skip: 0 },
+  });
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updatedPlaylist,
+        "video added to playlist successfully."
+      )
+    );
 });
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import {
+  addVideoToPlaylist,
   createPlaylist,
   getPlaylistById,
   getUserPlaylists,
@@ -8,12 +9,14 @@ import {
 
 const router = Router();
 
-router.use(verifyJwt);
+router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/").post(createPlaylist);
 
 router.route("/user/:userId").get(getUserPlaylists);
 
 router.route("/:playlistId").get(getPlaylistById);
+
+router.route("/add/:playlistId/:videoId").patch(addVideoToPlaylist);
 
 export default router;
