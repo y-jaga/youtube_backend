8317b77c0a2ce495142cdfaaf2c2ba17c3cdbe8f
y-jaga <yogeshjaga123@gmail.com>
2025-10-08 09:53:22 +0530

    feat: add healthcheck functionality with endpoint to verify service status; integrate healthcheck controller and routes

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
@@ -33,5 +34,6 @@ app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
